91d1384fa144f81c302853ad8c15cfb2882c5afc
eypbatihan <eypbatihan@gmail.com>
2021-10-31 23:39:31 +0300

    last

--- app.js
+++ app.js
@@ -1,29 +1,30 @@
+const text = document.getElementById("text").value;
+const add = document.querySelector(".add");
+const list = document.querySelector(".list");
 
-  const list = document.querySelector(".list")
-function add(){
+const todo = `<li class = "todo"><i class="fas fa-check"></i> ${text}<i class="fas fa-trash"></i></li>`;
+
+
+
+
+add.addEventListener("click",e =>{
     const text = document.getElementById("text").value;
-    
-    const html =`<li class="todo">
-
-    <i class="fas fa-check-circle"></i>
-
-    <span class = "span">${text}</span>
-
-<i class="fas fa-trash"></i>
-    </li>`;
-    list.innerHTML += html;
-  
-}
-list.addEventListener('click', e => {
-    if (e.target.classList.contains('fa-trash')) {
-      e.target.parentElement.remove();
+    const todo = `<li class = "todo"><i class="fas fa-check"></i> ${text}<i class="fas fa-trash"></i></li>`;
+    if(text != ""){
+    list.innerHTML += todo;
     }
-  });
-  list.addEventListener('click', e => {
-    if (e.target.classList.contains("fa-check-circle")) {
-      if ((e.target.parentElement).style.textDecoration != 'line-through')
-        (e.target.parentElement).style.textDecoration = 'line-through';
-      else (e.target.parentElement).style.textDecoration = 'none';
-    }
-  });
+   
+    });
 
+    list.addEventListener('click', e => {
+        if (e.target.classList.contains('fa-trash')) {
+          e.target.parentElement.remove();
+        }
+      });
+      list.addEventListener('click', e => {
+        if (e.target.classList.contains("fa-check")) {
+          if ((e.target.parentElement).style.textDecoration != 'line-through')
+            (e.target.parentElement).style.textDecoration = 'line-through';
+          else (e.target.parentElement).style.textDecoration = 'none';
+        }
+      });
